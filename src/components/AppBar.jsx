ba5976cf8d57eede4cde0b418c89fc6904b05580
Sheub <sebastien@sheub.eu>
2018-09-15 15:21:11 +0200

    Toggle language

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { I18n } from 'react-i18next';
+import { translate } from "react-i18next";
 
 import classNames from "classnames";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,12 +9,9 @@ import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import { withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import LanguageIcon from '@material-ui/icons/Language';
-
-
-
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import LanguageIcon from "@material-ui/icons/Language";
 
 import "./css/App.css";
 import "./css/mdIcons.css";
@@ -64,8 +61,7 @@ const drawerWidth = 270;
 class MyAppBar extends Component {
     state = {
         anchorEl: null,
-      };
-
+    };
 
     handleDrawerOpen = () => {
         this.props.handleDrawerOpen();
@@ -73,66 +69,67 @@ class MyAppBar extends Component {
 
     handleMenu = event => {
         this.setState({ anchorEl: event.currentTarget });
-      };
-    
-      handleClose = () => {
+    };
+
+    handleClose = () => {
         this.setState({ anchorEl: null });
-      };
+    };
 
     render() {
         const { open, classes } = this.props;
         const { anchorEl } = this.state;
         const openMenu = Boolean(anchorEl);
-        return (
-            <I18n ns="translations">
-                {
-                    (t, { i18n }) => (
-                        <div className={classes.root}>
+        const { t, i18n } = this.props;
 
-                            <CssBaseline />
-                            {/* ref={elem => (this.AppBar = elem)} */}
-                            <AppBar position="absolute" className={classNames(classes.appBar, open && classes.appBarShift)}>
-                                <Toolbar disableGutters={!open}>
-                                    <IconButton color="inherit" aria-label="Open drawer" onClick={this.handleDrawerOpen} className={classNames(classes.menuButton, open && classes.menuButtonHidden)}>
-                                        <MenuIcon />
-                                    </IconButton>
-                                    <Typography variant="title" color="inherit" noWrap className={classes.title}>
-                                        {t("title")}
-                                    </Typography>
-                                    <div>
-                                        <IconButton
-                                            aria-owns={open ? 'menu-appbar' : null}
-                                            aria-haspopup="true"
-                                            onClick={this.handleMenu}
-                                            color="inherit"
-                                        >
-                                            <LanguageIcon />
-                                        </IconButton>
-                                        <Menu
-                                            id="menu-appbar"
-                                            anchorEl={anchorEl}
-                                            anchorOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right',
-                                            }}
-                                            transformOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right',
-                                            }}
-                                            open={openMenu}
-                                            onClose={this.handleClose}
-                                        >
-                                            <MenuItem onClick={this.handleClose}>en</MenuItem>
-                                            <MenuItem onClick={this.handleClose}>fr</MenuItem>
-                                        </Menu>
-                                    </div>
-                                </Toolbar>
-                            </AppBar>
+        const changeLanguage = (lng) => {
+            this.setState({ anchorEl: null });
+            i18n.changeLanguage(lng);
+        }
+        return (
+
+            <div className={classes.root}>
+
+                <CssBaseline />
+                {/* ref={elem => (this.AppBar = elem)} */}
+                <AppBar position="absolute" className={classNames(classes.appBar, open && classes.appBarShift)}>
+                    <Toolbar disableGutters={!open}>
+                        <IconButton color="inherit" aria-label="Open drawer" onClick={this.handleDrawerOpen} className={classNames(classes.menuButton, open && classes.menuButtonHidden)}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" color="inherit" noWrap className={classes.title}>
+                            {t("title")}
+                        </Typography>
+                        <div>
+                            <IconButton
+                                aria-owns={open ? "menu-appbar" : null}
+                                aria-haspopup="true"
+                                onClick={this.handleMenu}
+                                color="inherit"
+                            >
+                                <LanguageIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                open={openMenu}
+                                onClose={this.handleClose}
+                            >
+                                <MenuItem onClick={() => changeLanguage("en")}>en</MenuItem>
+                                <MenuItem onClick={() => changeLanguage("fr")}>fr</MenuItem>
+                            </Menu>
                         </div>
-                    )
-                }
-            </I18n>
+                    </Toolbar>
+                </AppBar>
+            </div>
         );
     }
 }
-export default withStyles(styles)(MyAppBar);
+export default withStyles(styles)(translate("translations")(MyAppBar));
