c1ddda061552a3ddecd5a6b34bffb431235828b5
sheub <sebastien@sheub.eu>
2019-11-03 19:57:30 +0100

    Auth tidyup geocoder, add dependency and preparation for switch

--- src/components/Geocoder.js
+++ src/components/Geocoder.js
@@ -5,42 +5,46 @@ import {connect} from "react-redux";
 import MyPlaceName from "./MyPlaceName";
 import xhr from "xhr";
 
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
 /**
  * Geocoder component: connects to endpoint: 'https://api-adresse.data.gouv.fr
  * and provides an autocompleting interface for finding locations.
  */
 class Geocoder extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      results: [],
+      focus: null,
+      loading: false,
+      searchTime: new Date(),
+      geocode: false
+    };
 
-        this.state = {
-            results: [],
-            focus: null,
-            loading: false,
-            searchTime: new Date()
-        };
+    this.onInput = this.onInput.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onResult = this.onResult.bind(this);
+  }
 
-        this.onInput = this.onInput.bind(this);
-        this.onKeyDown = this.onKeyDown.bind(this);
-        this.onResult = this.onResult.bind(this);
-    }
+  componentDidMount() {
+    if (this.props.focusOnMount && this.input) this.input.focus();
+  }
 
-    componentDidMount() {
-        if (this.props.focusOnMount && this.input) this.input.focus();
-    }
-
-    onInput(e) {
-        this.setState({loading: true});
-        var value = e.target.value;
-        if (value === "") {
-            this.setState({
-                results: [],
-                focus: null,
-                loading: false
-            });
-        } else {
-            search(
+  onInput(e) {
+    this.setState({ loading: true });
+    var value = e.target.value;
+    if (value === "") {
+      this.setState({
+        results: [],
+        focus: null,
+        loading: false
+      });
+    } else {
+      search(
         this.props.endpoint,
         this.props.source,
         this.props.accessToken,
@@ -48,106 +52,121 @@ class Geocoder extends Component {
         this.props.bbox,
         this.props.types,
         value,
-        this.onResult);
-        }
-        this.props.writeSearch(value);
+        this.onResult
+      );
     }
+    this.props.writeSearch(value);
+  }
 
-    moveFocus(dir) {
-        if (this.state.loading) return;
-        this.setState({
-            focus: this.state.focus === null
-        ? 0 : Math.max(0,
-          Math.min(
-            this.state.results.length - 1,
-            this.state.focus + dir))
-        });
+  moveFocus(dir) {
+    if (this.state.loading) return;
+    this.setState({
+      focus:
+        this.state.focus === null
+          ? 0
+          : Math.max(
+              0,
+              Math.min(this.state.results.length - 1, this.state.focus + dir)
+            )
+    });
+  }
+
+  acceptFocus() {
+    if (this.state.focus !== null) {
+      this.props.onSelect(this.state.results[this.state.focus]);
     }
+  }
 
-    acceptFocus() {
-        if (this.state.focus !== null) {
-            this.props.onSelect(this.state.results[this.state.focus]);
-        }
-    }
-
-    onKeyDown(e) {
-        switch (e.which) {
+  onKeyDown(e) {
+    switch (e.which) {
       // up
-        case 38:
-            e.preventDefault();
-            this.moveFocus(-1);
-            break;
+      case 38:
+        e.preventDefault();
+        this.moveFocus(-1);
+        break;
       // down
-        case 40:
-            this.moveFocus(1);
-            break;
+      case 40:
+        this.moveFocus(1);
+        break;
       // accept
-        case 13:
-            if (this.state.results.length > 0 && this.state.focus === null) {
-                this.clickOption(this.state.results[0], 0);
-            }
-            this.acceptFocus();
-            break;
-
-        default:
-            break;
+      case 13:
+        if (this.state.results.length > 0 && this.state.focus === null) {
+          this.clickOption(this.state.results[0], 0);
         }
-    }
+        this.acceptFocus();
+        break;
 
-    onResult(err, res, body, searchTime) {
+      default:
+        break;
+    }
+  }
+
+  onResult(err, res, body, searchTime) {
     // searchTime is compared with the last search to set the state
     // to ensure that a slow xhr response does not scramble the
     // sequence of autocomplete display.
-        if (!err && body && body.features && this.state.searchTime <= searchTime) {
-            this.setState({
-                searchTime: searchTime,
-                loading: false,
-                results: body.features,
-                focus: null
-            });
-            this.props.onSuggest(this.state.results);
-        }
+    if (!err && body && body.features && this.state.searchTime <= searchTime) {
+      this.setState({
+        searchTime: searchTime,
+        loading: false,
+        results: body.features,
+        focus: null
+      });
+      this.props.onSuggest(this.state.results);
     }
+  }
 
-    clickOption(place, listLocation) {
-        this.props.onSelect(place);
-        this.setState({focus: listLocation});
+  clickOption(place, listLocation) {
+    this.props.onSelect(place);
+    this.setState({ focus: listLocation });
     // focus on the input after click to maintain key traversal
-        this.input.focus();
-        return false;
-    }
+    this.input.focus();
+    return false;
+  }
 
-    render() {
-        var input = <input
-      ref={(input) => { this.input = input; }}
-      className={this.props.inputClass}
-      onInput={this.onInput}
-      onKeyDown={this.onKeyDown}
-      value={this.props.searchString}
-      onChange={this.onInput}
-      placeholder={this.props.inputPlaceholder}
-      type='text'
-      aria-label="SearchInput"
-    />;
+  render() {
+    var input = (
+      <input
+        ref={input => {
+          this.input = input;
+        }}
+        className={this.props.inputClass}
+        onInput={this.onInput}
+        onKeyDown={this.onKeyDown}
+        value={this.props.searchString}
+        onChange={this.onInput}
+        placeholder={this.props.inputPlaceholder}
+        type="text"
+        aria-label="SearchInput"
+      />
+    );
 
-        return (
-      <div className='w-full'>
+    return (
+      <div className="w-full">
         {input}
         {this.state.results.length > 0 && this.props.searchString !== "" && (
           <ul className={this.props.resultsClass}>
             {this.state.results.map((result, i) => (
               <li
-                // key={result.id}
                 key={result.properties.id}
-                className={(i === this.state.focus ? "bg-blue-faint" : "bg-gray-faint-on-hover") + " h36 flex-parent flex-parent--center-cross pr12 cursor-pointer w-full w420-mm"}
+                className={
+                  (i === this.state.focus
+                    ? "bg-blue-faint"
+                    : "bg-gray-faint-on-hover") +
+                  " h36 flex-parent flex-parent--center-cross pr12 cursor-pointer w-full w420-mm"
+                }
                 onClick={this.clickOption.bind(this, result, i)}
               >
-                <div className='absolute flex-parent flex-parent--center-cross flex-parent--center-main w42 h42'>
-                  <svg className='icon color-darken25'><use xlinkHref='#icon-marker'></use></svg>
+                <div className="absolute flex-parent flex-parent--center-cross flex-parent--center-main w42 h42">
+                  <svg className="icon color-darken25">
+                    <use xlinkHref="#icon-marker"></use>
+                  </svg>
                 </div>
-                {/* <div className='pl42 pr12 txt-truncate' key={result.id}> */}
-                <div className='pl42 pr12 txt-truncate' key={result.properties.id}>
-                  <MyPlaceName location={result}/>
+                <div
+                  className="pl42 pr12 txt-truncate"
+                  key={result.properties.id}
+                >
+                  <MyPlaceName location={result} />
                 </div>
               </li>
             ))}
@@ -155,8 +174,19 @@ class Geocoder extends Component {
         )}
         {this.props.inputPosition === "bottom" && input}
       </div>
-        );
+    );
+  }
+
+  switchGeocoder(geocode) {
+    if (geocode) {
+      Geocoder.defaultProps.endpoint = "https://api-adresse.data.gouv.fr";
+      Geocoder.defaultProps.source = "api-adresse.data.gouv.fr";
+    } else {
+      Geocoder.defaultProps.endpoint = "http://localhost:8080/";
+      Geocoder.defaultProps.source = "autocomplete";
     }
+    this.setState({ geocode: geocode });
+  }
 }
 
 Geocoder.propTypes = {
@@ -176,9 +206,8 @@ Geocoder.propTypes = {
     searchString: PropTypes.string,
     writeSearch: PropTypes.func
 };
-// https://api-adresse.data.gouv.fr/search/?q=
+
 Geocoder.defaultProps = {
-  //endpoint: 'https://api.tiles.mapbox.com',
     endpoint: "https://api-adresse.data.gouv.fr",
     inputPosition: "top",
     inputPlaceholder: "Search",
@@ -189,26 +218,28 @@ Geocoder.defaultProps = {
     focusOnMount: true
 };
 
-function search(endpoint, source, accessToken, proximity, bbox, types, query, callback) {
+function search(endpoint, source, accessToken, proximity, bbox, types, query, callback) 
+{
   // Usually asynchronous calls would happen in the API caller,
   // but the results here are independent from the apps' state
-    var searchTime = new Date();
-  // var uri = endpoint + '/geocoding/v5/'
-    var uri = endpoint + "/search/?q="
-  //  + source + '/' + encodeURIComponent(query) + '.json'
-    + encodeURIComponent(query);
-    // + '?access_token=' + accessToken
-    // + (proximity ? '&proximity=' + proximity : '')
-    // + (bbox ? '&bbox=' + bbox : '')
-    // + (types ? '&types=' + encodeURIComponent(types) : '');
-    xhr({
-        uri: uri,
-        json: true
-    }, function (err, res, body) {
-        callback(err, res, body, searchTime);
-    });
+  var searchTime = new Date();
+  var uri = "";
+  if (source === "api-adresse.data.gouv.fr") {
+    uri = endpoint + "/search/?q=";
+  } else {
+    uri = endpoint + "autocomplete?q=";
+  }
+  uri = uri + encodeURIComponent(query);
+  xhr(
+    {
+      uri: uri,
+      json: true
+    },
+    function(err, res, body) {
+      callback(err, res, body, searchTime);
+    }
+  );
 }
-
 
 
 const mapStateToProps = (state) => {
