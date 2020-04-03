83a847b1c749041ecd4626e2f788291a023a7c9f
sheub <sebastien@sheub.eu>
2020-04-03 21:22:39 +0200

    eslint auth.js

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,24 +1,24 @@
 import { SET_AUTHENTICATED, SET_USER_DATA } from "../actions/auth";
 
-  // Default auth state
-  const defaultAuthState = {
-    user: null,
-    firstname: "",
-    lastname: "",
-    authenticated: false,
-  };
+// Default auth state
+const defaultAuthState = {
+  user: null,
+  firstname: "",
+  lastname: "",
+  authenticated: false,
+};
 
 const authReducer = (state = defaultAuthState, action) => {
   // export default (state = { user: null, authenticated: false }, action) => {
   switch (action.type) {
-    case SET_USER_DATA:
-      return Object.assign({}, state, { user: action.user });
-    case SET_AUTHENTICATED:
-      return Object.assign({}, state, {
-        authenticated: action.authenticated,
-      });
-    default:
-      return state;
+  case SET_USER_DATA:
+    return Object.assign({}, state, { user: action.user });
+  case SET_AUTHENTICATED:
+    return Object.assign({}, state, {
+      authenticated: action.authenticated,
+    });
+  default:
+    return state;
   }
 };
 export default authReducer;
