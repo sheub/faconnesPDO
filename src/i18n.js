7becb8d1754a9aa66a21a706ab1e0f09b5e68241
sheub <sebastien@sheub.eu>
2020-04-28 21:19:14 +0200

    correct i18n.js init

--- src/i18n.js
+++ src/i18n.js
@@ -5,23 +5,30 @@ import { reactI18nextModule } from "react-i18next";
 
 let baseDataUrl;
 if (process.env.NODE_ENV === "production") {
-    baseDataUrl = process.env.PUBLIC_URL;
+    baseDataUrl = process.env.REACT_APP_HOME;//"https://faconnes.de/";//process.env.PUBLIC_URL;
 } else { // Dev server runs on port 3000
     baseDataUrl = "http://localhost:3000/";
 }
-
+// Read persisted state from the local storage and put that in the initial state.
+var persistedS = localStorage.getItem("persistedState") ? JSON.parse(localStorage.getItem("persistedState")) : {};
+var languageSet = localStorage.getItem("i18nextLng") ? localStorage.getItem("i18nextLng") : "en";
+if (!(typeof (persistedS) !== "undefined" && typeof (persistedS.app) !== "undefined"))
+{
+  languageSet = "en"
+}
 i18n
   .use(Backend)
 //   .use(LanguageDetector)
   .use(reactI18nextModule)
   .init({
       fallbackLng: "en",
+      lng: languageSet,
 
     // have a common namespace used around the full app
       ns: ["translations"],
       defaultNS: "translations",
 
-      debug: true,
+      debug: false,
 
       interpolation: {
           escapeValue: false // react already safes from xss
